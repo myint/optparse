2bc1118533841d143db3a65d0a3612fe0b29b22c
Steven Myint <git@stevenmyint.com>
2015-10-09 08:24:30 -0700

    Move tests to script

---
+++ test.bash
@@ -0,0 +1,13 @@
+#!/bin/bash -eux
+
+trap "echo -e '\x1b[01;31mFailed\x1b[0m'" ERR
+
+make
+
+./test
+./test --more-milk
+# shellcheck disable=SC2065
+./test --invalid-option 2>&1 | grep 'no such option'
+./test --complex='(1.2, 3.4)' | grep 'complex: (1.2,3.4)'
+
+echo -e '\x1b[01;32mOK\x1b[0m'
