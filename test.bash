227b8533d86996f76b3e5b51d6e80abe2f1c44d9
Steven Myint <git@stevenmyint.com>
2015-12-08 06:06:17 -0800

    Test counting

--- test.bash
+++ test.bash
@@ -9,5 +9,6 @@ make
 # shellcheck disable=SC2065
 ./test --invalid-option 2>&1 | grep 'no such option'
 ./test --complex='(1.2, 3.4)' | grep 'complex: (1.2,3.4)'
+./test -kkk | grep '^k: 3$'
 
 echo -e '\x1b[01;32mOK\x1b[0m'
