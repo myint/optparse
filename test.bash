73ee6a7dbaa86c13fbfd850a36c68a4acbdcec11
Steven Myint <git@stevenmyint.com>
2015-12-08 06:23:08 -0800

    Test callback

--- test.bash
+++ test.bash
@@ -10,5 +10,6 @@ make
 ./test --invalid-option 2>&1 | grep 'no such option'
 ./test --complex='(1.2, 3.4)' | grep 'complex: (1.2,3.4)'
 ./test -kkk | grep '^k: 3$'
+./test --callback=foo | grep '^--- MyCallback --- val: foo$'
 
 echo -e '\x1b[01;32mOK\x1b[0m'
